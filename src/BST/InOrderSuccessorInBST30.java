ffc9c25c6e07eac4d45e4a9e8a12dc450a064bf1
ajaygadd <ajaygadd@qualcommlife.com>
2018-12-03 21:21:48 -0800

    BST

---
+++ src/BST/InOrderSuccessorInBST30.java
@@ -0,0 +1,35 @@
+package BST;
+
+public class InOrderSuccessorInBST30 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public Node process(Node node, Node target){
+        Node c = node;
+        Node next = null;
+        while(c!= null && c.value != target.value){
+            if(target.value < c.value){
+                node = c;
+                c = c.left;
+            }else{
+                c = c.right;
+            }
+        }
+
+        if(c == null){
+            return null;
+        }
+        if(c.right == null){
+            return next;
+        }
+        c = c.right;
+            while(c.left != null){
+                c = c.left;
+            }
+            return c;
+    }
+}
